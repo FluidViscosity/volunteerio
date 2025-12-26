269d5ff2949678dded49c7a0c8a1e593bafe332e
Tomer <tomer.simhony@gmail.com>
2025-12-26 19:43:12 +1300

    refactor: delete dead code

--- src/volunteerio/server.py
+++ src/volunteerio/server.py
@@ -1,20 +1,12 @@
 from flask import Flask
 
-# from flask_sqlalchemy import SQLAlchemy
 from dash_extensions.enrich import DashProxy, ServersideOutputTransform
 
 import dash_bootstrap_components as dbc
 
-# from db_config import design_info_modelling_db_url
-# from data_intelligence_platform.extensions import db
-
 
 def create_app():
     server = Flask("VolunteerIO")
-    # server.config["SQLALCHEMY_DATABASE_URI"] = design_info_modelling_db_url
-    # server.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    # db.init_app(server)
-
     app = DashProxy(
         __name__,
         server=server,
@@ -23,7 +15,4 @@ def create_app():
         # suppress_callback_exceptions=True,
         title="VolunteerIO",
     )
-    # with server.app_context():
-    #     db.create_all()
-
     return app, server
